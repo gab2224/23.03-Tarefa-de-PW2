f9ac097903e8ea26a64d1ac6d508278c1dee9e35
gab2224 <100096216+gab2224@users.noreply.github.com>
2022-08-24 08:55:50 -0300

    Add files via upload

---
+++ 24.08 PW2/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $('p').hide()
+
+    // Monitorar o click no botão "read"
+    $('#read').click(function(e){
+        e.preventDefault()
+
+        $('p').toggle(1500, function(){
+            // Criar uam Variavel para receber os status
+            let status = $('#read').attr('data-status')
+            // Limpando o label do btn
+            $('#read').empty()
+            // Testando o status 
+            if(status == 1 ){
+                $('#read').append('Esconder resumo')
+                $('#read').attr('data-status','2')
+            }else{
+                $('#read').append('Ver Resumo')
+                $('#read').attr('data-status','1')
+            }
+        })
+    })
+})
